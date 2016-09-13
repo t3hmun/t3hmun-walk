4875e1d7461c00ebdf435290ad23fcef87287254
mun <t3hmun@gmail.com>
2016-09-13 12:55:06 +0100

    v2.0.0 complete, has fildered walk function.

--- test.js
+++ test.js
@@ -1,40 +1,98 @@
+'use strict';
 // Creates some files and folders then runs walk an spams to console.
 // Test must be manually evaluated at console.
 // Also clean-up is manual.
-// It isn't brilliant, but it doesn't need to be.
+// It isn't brilliant, not TDD quality, but it doesn't need to be.
 
 const fs = require('fs');
 const path = require('path');
 const walk = require('./walk');
 
 // Make some folders and ignore any errors.
-[['./foldera', 'a1', 'a2', 'a3'],
-['./folderb', 'b1', 'b2', 'b3'],
-['./foldera/folderaa', 'aa1', 'aa2'],
-	].forEach(function(entry){
+console.log('\nTest-setup:\n');
+[
+    ['./test'],
+    ['./test/folderA', 'a1.keep', 'a2', 'a3.ignore'],
+    ['./test/folderB', 'b1.ignore', 'b2.keep', 'b3'],
+    ['./test/folderB/folderBB', 'bb1.ignore', 'bb2.keep', 'bb2'],
+    ['./test/folderA/folderAA', 'aa1.ignore', 'aa2.keep', 'aa3'],
+    ['./test/folderA/folderAA/folderAAA', 'aaa1.keep', 'aaa2', 'aaa3.ignore']
+].forEach(function (entry) {
     var dir;
-	entry.forEach(function(file, index){
-		if(index == 0) {
-			try{
-				fs.mkdirSync(file);
-			} catch(err) {}
-			dir = file;
-		}
-		else{
-			console.log('making ' + dir + '+' + "file");
-			try{
-				fs.closeSync(fs.openSync(path.join(dir, file), 'w'));
-			} catch(err) {}
-		}
-	});
+    entry.forEach(function (file, index) {
+        if (index == 0) {
+            try {
+                fs.mkdirSync(file);
+            } catch (err) {
+            }
+            dir = file;
+        }
+        else {
+            console.log('making ' + dir + '+' + "file");
+            try {
+                fs.closeSync(fs.openSync(path.join(dir, file), 'w'));
+            } catch (err) {
+            }
+        }
+    });
 });
 
-// Walk and then spam the console with results.
-walk('./', function(err, list){
-	if(err) throw err;
-	console.log('list len: ' + list.length);
-	list.forEach(function(item){
-		console.log("RES: " + item);
+new Promise(function (resolve, reject) {
+    // Walk and then spam the console with results.
+    console.log('\n\nAll:\n');
+    walk.all('./test', function (err, list) {
+        if (err) {
+            reject(err);
+            return;
+        }
+        console.log('list len: ' + list.length);
+        list.forEach(function (item) {
+            console.log("RES: " + item);
 
-	});
+        });
+        resolve();
+    });
+}).then(function () {
+    console.log('\n\nFiltered with only .keep files and skipping folderAA:\n');
+    return new Promise(function (resolve, reject) {
+        walk.where('./test/../test', dir=> dir == 'folderAA', file=> file.endsWith('.keep'), function (err, list) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            console.log('list len: ' + list.length);
+            list.forEach(function (item) {
+                console.log("RES: " + item);
+
+            });
+            resolve();
+        });
+    });
+}, function (err) {
+    throw err;
+}).then(function () {
+    console.log('\n\nFiltered with keep all files except .ignore and skipping folderAA with resolved path:\n');
+    return new Promise(function (resolve, reject) {
+        console.log('promise started.');
+        let dirPath = path.resolve('./test');
+        console.log(dirPath);
+        walk.where(dirPath, dir=> dir.endsWith('folderAA'), file=> !file.endsWith('.ignore'), function (err, list) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            console.log('list len: ' + list.length);
+            list.forEach(function (item) {
+                console.log("RES: " + item);
+
+            });
+            resolve();
+        });
+    });
+}, function (err) {
+    throw err;
+}).then(function () {
+    console.log('Completed successfully.')
+}, function (err) {
+    throw err;
 });
--- walk.js
+++ walk.js
@@ -1,21 +1,24 @@
 /** 
  * Asynchronous directory tree walking module for node.js v6
- * Uses promises to synchronise results from async io.
- * Results order should be considered random.
- * Directly exports the walk function.
+ * Results order should be considered random (async so no guarantees).
+ *
+ * Implementation:
+ * - Uses ES6 Promise to synchronise results from multiple callback chain branches.
+ * - Uses fs.readdir() and fs.stat() async io functions.
+ *
  */
 
 const fs = require('fs');
 const path = require('path');
 
 exports.all = walkAll;
+exports.where = walkWhere;
 
 /**
- * Asynchronously lists all files in specified dir and all subdirs.
+ * Asynchronously lists all files in specified dir and all sub-dirs.
  * 
  * @param {string} dir - Directory to start recursively listing files from.
- * @param {function(Error, string[])} callback - Either delivers an Error or a 
- *  list of files, paths joined with the original dir given.
+ * @param {function(Error, string[])} callback - Either delivers an Error or a list of files, paths joined with the original dir given (maintaining the relative or absolute nature of dir).
  */
 function walkAll(dir, callback) {
     fs.readdir(dir, function(err, files) {
@@ -34,30 +37,29 @@ function walkAll(dir, callback) {
         });
 
         // Closes on flatFiles and failed.
-        function addFileOrRecurse(filepath) {
+        function addFileOrRecurse(filePath) {
             return new Promise(function(resolve, reject) {
-                fs.stat(filepath, function(err, stat) {
+                fs.stat(filePath, function(err, stat) {
                     if (err) {
                         failed = true;
                         reject(err);
                         return;
                     }
                     if (stat.isFile()) {
-                        flatFiles.push(filepath);
+                        flatFiles.push(filePath);
                         resolve();
                         return;
                     }
                     if (stat.isDirectory()) {
                         // Recurse, resolving in callback, which is after the 
                         //  recursed walk() has resolved all its promises.
-                        walkAll(filepath, function(err, list) {
+                        walkAll(filePath, function(err, list) {
                             if (err) {
                                 failed = true;
                                 reject(err);
                                 return;
                             }
                             flatFiles.push(...list);
-                            // Final statement of the callback chain is resolve.
                             resolve();
                         });
                     }
@@ -69,8 +71,77 @@ function walkAll(dir, callback) {
             callback(null, flatFiles);
         }, () => {
             callback(err, null);
-        }).catch(function(err) {
-            throw err;
+        });
+    });
+}
+
+/**
+ * Recursively list file paths in the specified dir and all sub-directories that return true on predicates.
+ * If dirPredicates returns false on a dir path that folder and all sub-folders are skipped.
+ * The paths of files are simply path.join()ed onto the specified dir so relative dirs will remain relative.
+ * For example, if dir is './../../x/../x, a file path may be './../../x/sub-dir/file.txt', relative path is simplified.
+ * If absolute paths are required a the dir parameter should be given as an absolute path.
+ * @param {string} dir - The directory to start listing files recursively including sub-folders.
+ * @param {function(string) : boolean} dirPredicate - Returns true on directory paths (path not just directory) that should be included. The path may be relative or absolute depending on the dir parameter.
+ * @param {function(string) : boolean} filePredicate - Returns true for fileNames that should be included.
+ * @param {function(Error, string[])} callback - Either err or the list of files found, paths are joined onto dir.
+ */
+function walkWhere(dir, dirPredicate, filePredicate, callback) {
+    fs.readdir(dir, function(err, files) {
+        var flatFiles = [];
+        // Prevents more functions being called after a failure, not essential.
+        var failed = false;
+        if (err) {
+            callback(err, null);
+            return;
+        }
+        var promises = [];
+        // A promise resolved at the end of the callback chain for each path.
+        files.some(function(file) {
+            promises.push(addFileOrRecurse(path.join(dir, file)));
+            return failed; // if failed == true break;
+        });
+
+        // Closes on flatFiles and failed.
+        function addFileOrRecurse(filePath) {
+            // Promises are a part of ES6.
+            return new Promise(function(resolve, reject) {
+                fs.stat(filePath, function(err, stat) {
+                    if (err) {
+                        failed = true;
+                        reject(err);
+                        return;
+                    }
+                    if (stat.isFile() ) {
+                        if(filePredicate(filePath)) flatFiles.push(filePath);
+                        resolve();
+                        return;
+                    }
+                    if (stat.isDirectory()) {
+                        if(dirPredicate(filePath)){
+                            resolve();
+                            return;
+                        }
+                        // Recurse, resolve() in callback, after the inner walk() has resolved all its promises.
+                        walkWhere(filePath, dirPredicate, filePredicate, function(err, list) {
+                            if (err) {
+                                failed = true;
+                                reject(err);
+                                return;
+                            }
+                            flatFiles.push(...list); //ES6 spread operator.
+                            resolve();
+                        });
+                    }
+                });
+            });
+        }
+
+        // Promise.all: When everything has succeeded or any one has failed.
+        Promise.all(promises).then(() => {
+            callback(null, flatFiles);
+        }, () => {
+            callback(err, null);
         });
     });
 }
